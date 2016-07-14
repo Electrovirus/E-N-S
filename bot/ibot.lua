a243afe41c3b8a00086e569f08201d9b95c3bae4
Electrovirus <E.electrovirus@gmail.com>
2016-07-14 12:05:09 +0430

    Update and rename shieldbot.lua to ibot.lua

--- bot/ibot.lua
+++ bot/ibot.lua
@@ -253,26 +253,7 @@ function create_config( )
     },
     sudo_users = {80182995},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[MeGa shield v2.9
-An advanced administration bot based on TG-CLI written in Lua
-
-Github:
-https://github.com/hafez16/mega-shield
-
-Admins:
-@hafez1116hafez [Developer]
-@amirho3ien911 [Developer]
-@Eblis_alone [Manager]
-@Mmd_hei [sponsor]
-
-Special thanks to
-SEEDTEAM
-Hextor team
-Magic team
-Avira team
-
-Our channels
-@shieldTM [persian]
+    about_text = [[ibot based on teleseed by Electrovirus
 ]],
     help_text_realm = [[
 Realm Commands:
