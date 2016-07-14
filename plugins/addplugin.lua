6db4e8064bb1322fcbb5e14e3650a38edd3d65c9
Electrovirus <E.electrovirus@gmail.com>
2016-07-14 12:10:14 +0430

    Update addplugin.lua

--- plugins/addplugin.lua
+++ plugins/addplugin.lua
@@ -6,20 +6,20 @@ local function run(msg, matches)
     text,b = text:gsub('^!+','')
   end
     if not is_sudo(msg) then
-    return 'ADD PLUGINS ONLY BY SUDO😁'
+    return 'ADD PLUGINS ONLY BY SUDO✘'
   end
   local name = matches[2]
   local file = io.open("./"..name, "w")
   file:write(text)
   file:flush()
   file:close()
-  return "done😊"
+  return "done✔"
  end
  return {
   description = "a Usefull plugin for sudo !",
   usage = "A plugins to add Another plugins to the server",
   patterns = {
-    "^[/#!]plugin (.+) (.*)$"
+    "^[/#!]addplug (.+) (.*)$"
   },
   run = run
 }
