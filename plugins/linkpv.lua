d10c615a3a209e1e9cccadc12029a2ba97633e11
Electrovirus <E.electrovirus@gmail.com>
2016-07-29 00:21:43 +0430

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,16 +2,16 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "مجاز برای مدیر و ادمین ها"
+        return "Sudo only🗽"
        end
     local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
-        return "اول باید لینک جدید ایجاد کنید"
+        return "Create a newlink first🗽"
        end
-         local text = "لینک گروه:\n"..group_link
+         local text = "☞Group link:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text, ok_cb, false)
-           return "لینک به pv ارسال شد"
+           return "Link was sent to pv🔰"
 end
 
 return {
