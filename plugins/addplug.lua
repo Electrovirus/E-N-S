b8187e1b47bea86bc2bab6cca30ee3ed613d074c
Electrovirus <E.electrovirus@gmail.com>
2016-07-29 00:59:15 +0430

    Create addplug.lua

---
+++ plugins/addplug.lua
@@ -0,0 +1,26 @@
+do
+ local function save_file(name, text)
+    local file = io.open("./plugins/"..name, "w")
+    file:write(text)
+    file:flush()
+    file:close()
+    return "فایل ارسالی در سرور ذخیره شد"
+end   
+function run(msg, matches)
+  if matches[1] == "addplugin" and is_sudo(msg) then
+ 
+         local name = matches[2]
+        local text = matches[3]
+        return save_file(name, text)
+        end
+        if not is_sudo(msg) then 
+		return "فقد برای ســـــودو ها"
+	end
+end
+return {
+  patterns = {
+  "^[!/#](addplug) ([^%s]+) (.+)$"
+  },
+  run = run
+}
+end
