0d0597783cece5855cb1d3cccb04586bd06007eb
Electrovirus <E.electrovirus@gmail.com>
2016-07-29 15:08:07 +0430

    Update sharecontact.lua

--- plugins/sharecontact.lua
+++ plugins/sharecontact.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+19122090223", "»│E N S│«", "", ok_cb, false)
+send_contact(get_receiver(msg), "+19122090223", "E N S", "", ok_cb, false)
 end
 
 return {
