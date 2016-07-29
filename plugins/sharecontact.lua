c2157285b2d5bc66e49894127218fa34b8e4250f
Electrovirus <E.electrovirus@gmail.com>
2016-07-29 14:17:46 +0430

    Create sharecontact.lua

---
+++ plugins/sharecontact.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+19122090223", "»│E N S│«", "", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/#]share$"
+
+},
+run = run
+}
+
+end
