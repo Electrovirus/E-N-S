3458f86163c47367833e2a0fdee4cf642029ce61
Electrovirus <E.electrovirus@gmail.com>
2016-08-02 22:30:58 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -143,7 +143,7 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (shield)",
+    "^[#!/]([Ss]tats) (ens)",
 	"^[#!/]([Ee]ns)"
     }, 
   run = run
