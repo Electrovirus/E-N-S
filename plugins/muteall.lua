8e88999d34052bf2d71aef7cb0122692e7b8556c
Electrovirus <E.electrovirus@gmail.com>
2016-08-08 21:33:16 +0430

    Create muteall.lua

---
+++ plugins/muteall.lua
@@ -0,0 +1,37 @@
+do
+local function pre_process(msg)
+ local hash = 'muteall:'..msg.to.id
+  if redis:get(hash) and msg.to.type == 'channel' and not is_momod(msg)  then
+   delete_msg(msg.id, ok_cb, false)
+       end
+    return msg
+ end
+ 
+local function run(msg, matches)
+ if matches[1] == 'muteall' and is_momod(msg) then
+       local hash = 'muteall:'..msg.to.id
+       if not matches[2] then
+              redis:set(hash, true)
+             return "mute all has been enabled"
+ else
+ local num = tonumber(matches[2]) * 60
+ redis:setex(hash, num, true)
+ return "mute all has been enabled for |#"..matches[2].."#| minutes"
+ end
+ end
+if matches[1] == 'unmuteall' and is_momod(msg) then
+               local hash = 'muteall:'..msg.to.id
+        redis:del(hash)
+          return "mute all has been disabled"
+  end
+end
+return {
+   patterns = {
+      '^[/!#](muteall)$',
+      '^[/!#](unmuteall)$',
+   '^[/!#](muteall) (%d+)$',
+ },
+run = run,
+  pre_process = pre_process
+}
+end
