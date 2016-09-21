0cbb0dd9f1b55a2ac379b3d20b593987aaf9f060
Electrovirus <E.electrovirus@gmail.com>
2016-09-21 19:12:47 +0330

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,12 +1,12 @@
 do
 function run(msg, matches)
-  return "════════════════════════\n👤"..msg.from.first_name.."━"..msg.from.last_name.."\n📶"..msg.from.username.."\n🆔"..msg.from.id.."\n════════════════════════\n👥"..msg.to.title.."\n🆔"..msg.to.id.."\n════════════════════════"
+  return "════════════════════════\n👤"..msg.from.first_name.."━"..msg.from.last_name.."\n📶@"..msg.from.username.."\n🆔"..msg.from.id.."\n════════════════════════\n👥"..msg.to.title.."\n🆔"..msg.to.id.."\n════════════════════════"
 end
 return {
   description = "info", 
   usage = "info",
   patterns = {
-    "^[!#/]([Ii][Dd])$",
+    "^[!#/]([Ii][Dd][Ss])$",
   },
   run = run
 }
