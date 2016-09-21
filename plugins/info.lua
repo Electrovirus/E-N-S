27963e087fdb08059b70d0fedca4cb0726c16f79
Electrovirus <E.electrovirus@gmail.com>
2016-09-22 01:19:00 +0330

    E-N-S V.2

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,6 +1,5 @@
 do
 function run(msg, matches)
-send_document(get_receiver(msg),'file/PowerUP.webp',ok_cb,false)
   return "🤖BOT NAME : 🌟E N S🌟\n👥GROUP NAME : "..msg.to.title.."\n➖➖➖➖➖➖➖➖➖➖➖\n🗣NAME : "..(msg.from.first_name or '-').."\n🌟`USERNAME` : @"..(msg.from.username or '-').."\n🆔ID : "..msg.from.id.."\n➖➖➖➖➖➖➖➖➖➖➖\n📱PHONENUMBER : +"..(msg.from.phone or 'Error 404').."\n➖➖➖➖➖➖➖➖➖➖➖\n🏵YOUR LINK : telegram.me/"..(msg.from.username or '-').."\n"
 end
 return {
