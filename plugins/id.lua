4ae60ea181582990934d16dfd3a8f8bd21d2edbc
Electrovirus <E.electrovirus@gmail.com>
2016-09-21 18:50:25 +0330

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,11 +3,12 @@ function run(msg,match)
 if matches[1] == "id" then
  return "════════════════════════\n👤"..msg.from.first_name.."━"..msg.from.last_name.."\n📶"..msg.from.username.."\n🆔"..msg.from.id.."\n════════════════════════\n👥"..msg.to.title.."\n🆔"..msg.to.id.."\n════════════════════════"
 end
+end
 return {
   description = "id", 
   usage = "id",
   patterns = {
-    "^[!#/](.*)$",
+    "^[!#/](id)$",
   },
   run = run
 }
