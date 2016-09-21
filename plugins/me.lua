f5a168e322a99ae487ca1f3c5b2c6eb1e5e4c695
Electrovirus <E.electrovirus@gmail.com>
2016-09-21 18:37:56 +0330

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -56,7 +56,7 @@ end
       local hashs = 'msgs:'..result.peer_id..':'..extra.msg.to.id
       local msgss = redis:get(hashs)
       local percent = msgss / r * 100
-    return send_large_msg(chatid, "🌀Your Name "..name.."\n➖➖➖➖➖➖➖➖➖➖\n🌀The number of messages you ( "..msgss.." ) {"..round2(percent).."/💯}\n➖➖➖➖➖➖➖➖➖➖\n  🌀The number of messages in the group "..r.."")
+    return send_large_msg(chatid, "🌎Your Name "..name.."\n➖➖➖➖➖➖➖➖➖➖\n🌎The number of messages you ( "..msgss.." ) {"..round2(in).."/💯}\n➖➖➖➖➖➖➖➖➖➖\n🌎The number of messages in the group "..r.."")
 
     end
 end
