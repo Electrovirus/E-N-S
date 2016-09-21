a30db673e7e209b8430902fc9d2cdc8d87dd2687
Electrovirus <E.electrovirus@gmail.com>
2016-09-22 01:47:27 +0330

    E-N-S V.2

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -5,21 +5,21 @@ local function history(extra, suc, result)
   if tonumber(extra.con) == #result then
     send_msg(extra.chatid, '"'..#result..'"Recent group messages have been cleared', ok_cb, false)
   else
-    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
+    send_msg(extra.chatid, '☑Messages cleared', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] == 'clean' and is_owner(msg) then
     if msg.to.type == 'channel' then
       if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
-        return "تعداد بیشتر از 1 مجاز است"
+        return "🎌Selected messages must be more than 1"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "فقط در سوپرگروه ممکن است"
+      return "📌Only in supergroups"
     end
   else
-    return "شما دسترسی ندارید"
+    return "👻You do not have acces"
   end
 end
 
