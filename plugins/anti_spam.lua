d74e686330ade536fe8369b2921dbdea7bb937bc
Electrovirus <E.electrovirus@gmail.com>
2016-09-22 00:46:18 +0330

    E-N-S V.2

--- plugins/anti_spam.lua
+++ plugins/anti_spam.lua
@@ -108,10 +108,10 @@ local function pre_process(msg)
 	  if msg.to.type == 'chat' or msg.to.type == 'channel' then
 		if username then
 			savelog(msg.to.id, name_log.." @"..username.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\n@"..username.."["..msg.from.id.."]\nStatus: User kicked")
+			send_large_msg(receiver , "🆘User "..name_log.." 〈"..msg.from.id.."〉\n⚠️kicked from 《"..msg.to.id.."》\n⛔️Reason∵Flooding")
 		else
 			savelog(msg.to.id, name_log.." ["..msg.from.id.."] kicked for #spam")
-			send_large_msg(receiver , "Flooding is not allowed here\nName:"..name_log.."["..msg.from.id.."]\nStatus: User kicked")
+			send_large_msg(receiver , "🆘User "..name_log.." 〈"..msg.from.id.."〉\n⚠️kicked from 《"..msg.to.id.."》\n⛔️Reason∵Flooding")
 		end
 	  end
       -- incr it on redis
