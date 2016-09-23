b7e3310597f9d6c131a034c0677a3888cb3a8514
Electrovirus <E.electrovirus@gmail.com>
2016-09-23 04:55:16 +0330

    E-N-S V.2

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, "Done✔", ok_cb, false)
+		return "🔄Bot is Reloaded!\n@ENS_Tg"
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
