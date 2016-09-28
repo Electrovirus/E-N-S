8e3471da95e14eab7ed32e5f80240d5868f4bed2
Electrovirus <E.electrovirus@gmail.com>
2016-09-28 18:05:35 +0330

    E-N-S V.2

--- plugins/lock_inline.lua
+++ plugins/lock_inline.lua
@@ -9,12 +9,12 @@ end
     end
     
  local function run(msg, matches)
-if is_momod(msg) and matches[1] == "u" and matches[2] == "inline" then
+if is_momod(msg) and matches[1] == "unlock" and matches[2] == "inline" then
 local inline = 'mate:'..msg.to.id
 redis:set(inline, true)
 return "🔓Inline is unlocked in this group By : @"..msg.from.username.." "
 end
-if is_momod(msg) and matches[1] == "l" and matches[2] == "inline" then
+if is_momod(msg) and matches[1] == "lock" and matches[2] == "inline" then
     local inline = 'mate:'..msg.to.id
     redis:del(inline)
     return "🔒Inline is locked in this group By : @"..msg.from.username.." "
@@ -22,8 +22,8 @@ if is_momod(msg) and matches[1] == "l" and matches[2] == "inline" then
 end
 return {
     patterns ={
-        '^[/!#](u)(inline)$',
-        '^[/!#](l)(inline)$',
+        '^[/!#](unlock) (inline)$',
+        '^[/!#](lock) (inline)$',
     },
 run = run,
 pre_process = pre_process 
