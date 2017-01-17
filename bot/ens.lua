9b0d6ca99f05f057f9c7df9db121961dfff5de02
rezablack077 <oxford_sajadi@hotmail.com>
2017-01-17 05:24:00 +0000

    Update ens.lua

--- bot/ens.lua
+++ bot/ens.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {184018132},--Sudo users
+    sudo_users = {193068389},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Electrovirus national security bot🌐
  E N S
